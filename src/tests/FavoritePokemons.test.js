d22c31072a67a9d46efdf1c5784a7a7c671c346a
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-09-20 14:48:53 -0300

    Requisito3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemons';
+
+describe('Testes do componente Favorite Pokémon', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const noFavorite = screen.getByText(/no favorite pokemon found/i);
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
